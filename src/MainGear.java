587466018d2ebde83fea1161efef2e2d656992df
akloand <adidas2783>
2022-05-10 22:21:12 +0200

    10/05/2022

---
+++ src/MainGear.java
@@ -0,0 +1,11 @@
+import All.GearBox;
+
+public class MainGear {
+    public static void main(String[] args) {
+        GearBox gearBox = new GearBox();
+
+        gearBox.getCurrentGear();
+
+    }
+}
+
