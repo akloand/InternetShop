766f74f805067a0d0b9583df35bfe22e86e7d076
akloand <akloand@gmail.com>
2022-05-11 16:55:02 +0200

    11/05/2022

---
+++ src/MainAirport.java
@@ -0,0 +1,23 @@
+import com.skillbox.airport.Airport;
+
+public class MainAirport {
+
+    public static void main(String[] args) {
+        Airport airport = Airport.getInstance();
+
+        System.out.println("Терминалы: " + airport.getTerminals());
+        System.out.println("Самолеты: " + airport.getAllAircrafts());
+
+        int lenghtJet = airport.getAllAircrafts().toArray().length;
+        int lenghtTerminal = airport.getTerminals().toArray().length;
+        int arrayJet[] = new int[lenghtJet];
+        System.out.println("Количество терминалов: "+lenghtJet);
+        System.out.println("Количество самолетов: "+lenghtTerminal);
+//        for (int i = 0; i < lenghtJet ; i++) {
+//            arrayJet[i] = airport.getAllAircrafts();
+//
+//        }
+
+    }
+}
+
